03617f69e7237fa3a05768d8e6a62d221f0d55a3
muahm <muahm@OL-MOHALI>
2024-02-23 20:00:17 +0300

    add 14. LongestCommonPrefix

---
+++ 14. LongestCommonPrefix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+Console.WriteLine(LongestCommonPrefix(new string[] { "flower", "flow", "flight" }));
+string LongestCommonPrefix(string[] strs)
+{
+    Dictionary<int, char> dict = new();
+    Dictionary<int, char> dub = new();
+
+    for (int i = 0; i < strs.Length; i++)
+    {
+        char[] letter = strs[i].ToCharArray();
+        for (int j = 0; j < letter.Length; j++)
+        {
+            if (!dict.ContainsKey(letter[j]))
+                dict.Add((int)letter[j], (char)letter[j]);
+            else
+                dub.Add((int)letter[j], (char)letter[j]);
+        }
+    }
+    return strs[0];
+}
