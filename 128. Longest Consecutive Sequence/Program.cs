68744ec3cac41c838a7d0d400dac19556d2ea873
muahm <muahm@OL-MOHALI>
2024-02-23 20:04:45 +0300

    add Assignment

---
+++ 128. Longest Consecutive Sequence/Program.cs
@@ -0,0 +1,50 @@
+﻿int[] nums = new int[] { 100, 4, 200, 1, 3, 2 };
+
+Console.WriteLine(LongestConsecutive(nums));
+int LongestConsecutive(int[] nums)
+{
+    int[] consecutiveElements = new int[10];
+    int index = 0;
+    int Fsequence = 0;
+    int Lsequence = 0;
+    nums = SelectionSort(nums.ToList());
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (Fsequence != nums[i] - nums[i - 1])
+        {
+            Fsequence = nums[i] - nums[i - 1];
+            index++;
+        }
+        if (nums[i] - Fsequence == nums[i - 1])
+        {
+            consecutiveElements[index] += 1;
+        }
+
+    }
+    return consecutiveElements[index];
+}
+static int[] SelectionSort(List<int> arr)
+{
+    var newArr = new int[arr.Count];
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        var smallest = FindSmallest(arr);
+        newArr[i] = arr[smallest];
+        arr.RemoveAt(smallest);
+    }
+    return newArr;
+}
+static int FindSmallest(List<int> arr)
+{
+    var smallest = arr[0];
+    var smallestIndex = 0;
+    for (int i = 1; i < arr.Count; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            smallest = arr[i];
+            smallestIndex = i;
+        }
+    }
+    return smallestIndex;
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,47 @@
+﻿// Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
+// Examples
+//removeDups([1, 5, 1, 0, 2, 5]) ➞ [1, 5, 0, 2]
+//removeDups(["The", "big", "cat", "big"]) ➞ ["The", "big", "cat"]
+//removeDups(["Sky", "is", "blue"]) ➞ ["Sky", "is", "blue"]
+//removeDups(["John", "Taylor", "John"]) ➞ ["John", "Taylor"] 
+//int[] nums1 = new int[] { 1, 5, 1, 0, 2, 5 };
+//Console.WriteLine(string.Join(",", RemoveDublicate(nums1)));
+
+//IEnumerable<T> RemoveDublicate<T>(IEnumerable<T> arr)
+//{
+//    HashSet<T> newArr = new ();
+//    foreach (var item in arr)
+//    {
+//        if (!newArr.Add(item))
+//        {
+
+//        }
+//    }
+//    return newArr;
+//}
+
+//Console.WriteLine(CalcPageNumber(8, 9));
+//int CalcPageNumber(int n, int page)
+//{
+//    int maxMove = page / 2;
+//    int move = n / 2;
+//    return maxMove;
+//}
+
+int[] nums = new int[] { 1, -2, -8, 4, 5 };
+Console.WriteLine(ComputeClosestToZero(nums));
+int ComputeClosestToZero(int[] ts)
+{
+    if (ts.Length is default(int))
+        return default;
+
+    int closestTemp = ts[0];
+    for (int i = 1; i < ts.Length; i++)
+    {
+        if (Math.Abs(ts[i]) < Math.Abs(closestTemp))
+            closestTemp = ts[i];
+        else if (Math.Abs(ts[i]) == Math.Abs(closestTemp))
+            closestTemp = ts[i];
+    }
+    return closestTemp;
+}
