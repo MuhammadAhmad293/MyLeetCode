ac375111bf001809123b7f22176c2122ca9ac25f
muahm <muahm@OL-MOHALI>
2024-02-23 19:42:00 +0300

    change name

---
+++ 1-TwoSum/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] nums = new int[] { 2, 7, 11, 15 };
+int target = 25;
+Console.WriteLine(string.Join(",", TwoSum(nums, target)));
+
+int[] TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+                return new int[] { i, j };
+        }
+    }
+    return nums;
+}
