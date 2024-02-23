8ceb70c87a0c513a6918d4276867e908c1332ab9
muahm <muahm@OL-MOHALI>
2024-02-23 20:03:10 +0300

    add 88-MergeSortedArray

---
+++ 88-MergeSortedArray/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+int[] nums2 = new int[] { 2, 5, 6 };
+int m = 3;
+int n = 3;
+Merge(nums1, m, nums2, n);
+
+static void Merge(int[] nums1, int m, int[] nums2, int n)
+{
+    int mi = m - 1;
+    int ni = n - 1;
+    int len = m + n - 1;
+
+    while (mi >= 0 || ni >= 0)
+    {
+        if (ni < 0)
+            break;
+        if (mi >= 0 && ni >= 0)
+            nums1[len--] = nums1[mi] > nums2[ni] ? nums1[mi--] : nums2[ni--];
+        else
+            nums1[len--] = nums2[ni--];
+    }
+}
+
