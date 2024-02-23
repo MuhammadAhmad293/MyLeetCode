f86e0f6f51f6b66d653871815fa68a583d74e94f
muahm <muahm@OL-MOHALI>
2024-02-23 19:44:09 +0300

    add 9. PalindromeNumber

---
+++ 9. PalindromeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(IsPalindrome(-121));
+
+static bool IsPalindrome(int x)
+{
+    int r, sum = 0, target;
+    for (target = x; x > 0; x = x / 10)
+    {
+        r = x % 10;
+        sum = sum * 10 + r;
+    }
+    return target == sum;
+}
